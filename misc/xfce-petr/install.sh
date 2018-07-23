312fd100f796aca58eef16301be5858ef6ac06a5
comrob <petr.cizek@fel.cvut.cz>
2018-07-23 16:36:34 +0200

    installation of icon theme

--- misc/xfce-petr/install.sh
+++ misc/xfce-petr/install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+#install revival icon theme
+cd /tmp
+wget https://www.dropbox.com/s/abnmjp2k828dcmw/revival-icon-theme.tar.gz
+tar -xvf revival-icon-theme.tar.gz
+cp -r revival /usr/share/icons
 
