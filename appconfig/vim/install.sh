3f561da6ee5e16759001accacdbe1c77f5715aba
petr <cizekpe6@fel.cvut.cz>
2018-11-07 16:27:07 +0100

    adjusted .vimrc and added snippets vim files

--- appconfig/vim/install.sh
+++ appconfig/vim/install.sh
@@ -12,7 +12,7 @@ APP_PATH=`( cd "$APP_PATH" && pwd )`
       --enable-multibyte \
       --enable-rubyinterp=yes \
       --enable-python3interp=yes \
-      --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+      --with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu \
       --enable-perlinterp=yes \
       --enable-luainterp=yes \
       --enable-gui=gtk2 --enable-cscope --prefix=/usr
