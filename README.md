41517268d29a8444d601c755df41f35ffa24f49d
petrc <petr.cizek@fel.cvut.cz>
2021-11-18 09:10:47 +0100

    added git workflows

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # linux-setup
+
+| Ubuntu               | Architecture | Status                                                                                                                                                           |
+| -------------------- | ------------ | ---------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| 18.04 Bionic         | AMD64        | [![Build Status](https://github.com/klaxalk/linux-setup/workflows/Bionic/badge.svg)](https://github.com/xpetresx/linux-setup/actions)                            |
+| 20.04 Focal          | AMD64        | [![Build Status](https://github.com/klaxalk/linux-setup/workflows/Focal/badge.svg)](https://github.com/xpetresx/linux-setup/actions)                             |
+
--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ cd $MY_PATH
 git pull
 
 #initialize submodules
-git submodule init 
 git submodule update --init --recursive
 
 #update and upgrade current packages
@@ -19,25 +18,22 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 
 #install essentials
-sudo apt-get -y install build-essential make cmake cmake-curses-gui ccache pkg-config automake autoconf libc++-dev clang-6.0 clang-format-6.0 python2.7-dev python3-dev python3-pip
-
-#install libs
-sudo apt-get -y install libncurses5-dev libsdl2-dev
+sudo apt-get -y install build-essential make cmake cmake-curses-gui ccache pkg-config automake autoconf libc++-dev clang clang-format
 
 #install tools
-sudo apt-get -y install htop tree ncdu git subversion unzip dialog figlet toilet exuberant-ctags xclip xsel
+sudo apt-get -y install htop tree ncdu git subversion unzip exuberant-ctags xclip xsel
 
 #install cifs-utils for mounting dataset repository
-sudo apt-get -y install cifs-utils exfat-fuse
+sudo apt-get -y install cifs-utils exfat-fuse exfat-utils
 
 #install programs
 #sudo apt-get -y install vlc mplayer ffmpeg okular krita imagemagick
 
 #install tmux
-bash $APPCONFIG_PATH/tmux/install.sh
+#bash $APPCONFIG_PATH/tmux/install.sh
 
 #install vim
-bash $APPCONFIG_PATH/vim/install.sh
+#bash $APPCONFIG_PATH/vim/install.sh
 
 #install opencv
 #bash $APPCONFIG_PATH/opencv/install.sh
@@ -46,23 +42,23 @@ bash $APPCONFIG_PATH/vim/install.sh
 #bash $APPCONFIG_PATH/tex/install.sh
 
 #install ros
-bash $APPCONFIG_PATH/ros/install.sh
+#bash $APPCONFIG_PATH/ros/install.sh
 
 #install python libs
-bash $APPCONFIG_PATH/python/install.sh
+#bash $APPCONFIG_PATH/python/install.sh
 
 #add aliases
-sudo ln -s /usr/bin/xdg-open /usr/bin/open
+#sudo ln -s /usr/bin/xdg-open /usr/bin/open
 
 #add tmux startup if it is not already in .bashrc 
-num=`cat ~/.bashrc | grep "RUN_TMUX" | wc -l`
-if [ "$num" -lt "1" ]; then
-    cat $APPCONFIG_PATH/tmux/bashconfig >> ~/.bashrc
-fi
+#num=`cat ~/.bashrc | grep "RUN_TMUX" | wc -l`
+#if [ "$num" -lt "1" ]; then
+#    cat $APPCONFIG_PATH/tmux/bashconfig >> ~/.bashrc
+#fi
 
 #add user to dialout group
-sudo adduser `whoami` dialout
-sudo apt-get -y install gtkterm
+#sudo adduser `whoami` dialout
+#sudo apt-get -y install gtkterm
 
 #copy udev-rules
 #bash $MISC_PATH/udev/install.sh 
