347869f57d23173ce5395566fcf0c51e20e138ca
petrc <petr.cizek@fel.cvut.cz>
2021-11-24 10:47:19 +0100

    added installation of ranger

--- appconfig/ranger/install.sh
+++ appconfig/ranger/install.sh
@@ -4,5 +4,15 @@
 APP_PATH=`dirname "$0"`
 APP_PATH=`( cd "$APP_PATH" && pwd )`
 
+#install prerequisites
+sudo apt-get -y install caca-utils libimage-exiftool-perl w3m w3m-img
+
 #install ranger
-sudo apt-get -y install ranger
+( cd $APP_PATH/../../submodules/ranger && sudo make install )
+
+#configure ranger
+mkdir -p ~/.config/ranger
+ln -fs $APP_PATH/rifle.conf ~/.config/ranger/rifle.conf
+ln -fs $APP_PATH/commands.py ~/.config/ranger/commands.py
+ln -fs $APP_PATH/rc.conf ~/.config/ranger/rc.conf
+ln -fs $APP_PATH/scope.sh ~/.config/ranger/scope.sh
--- misc/xfce-petr/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#install revival icon theme
-cd /tmp
-wget https://www.dropbox.com/s/abnmjp2k828dcmw/revival-icon-theme.tar.gz
-tar -xvf revival-icon-theme.tar.gz
-cp -r revival /usr/share/icons
-
